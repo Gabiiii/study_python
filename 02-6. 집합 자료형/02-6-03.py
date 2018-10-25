0bb186b400257d81fadf9580fae41821f5167415
Gabiiii <anna9570@gmail.com>
2018-10-25 11:32:22 +0900

    02-6 집합 자료형

---
+++ 02-6. 집합 자료형/02-6-03.py
@@ -0,0 +1,4 @@
+s1 = set(['a', 'b', 'c', 'd', 'e'])
+s2 = set(['c', 'd', 'e', 'f', 'g'])
+print(s1-s2)
+print(s1.difference(s2))
---
+++ 02-6. 집합 자료형/02-6-04.py
@@ -0,0 +1,6 @@
+a = {'a', 'b', 'c'}
+print(a)
+print(type(a))
+b = set()
+print(b)
+print(type(b))
